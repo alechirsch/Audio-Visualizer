be02506544e0fa5d9dc672ac495e087329f4d2b5
Alec <arhirsch@umass.edu>
2015-04-14 15:05:50 -0400

    refactored the the html

--- server.js
+++ server.js
@@ -3,8 +3,13 @@ var fs = require('fs');
 var ytdl = require('ytdl-core');
 var ejs = require('ejs');
 var Path = require('path');
-var server = new Hapi.Server();
-server.connection( { port: 8000 } );
+var server = new Hapi.Server('0.0.0.0', process.env.PORT || 8000, {
+    "payload": {
+        "maxBytes": 104857600 //Increasing max file size to 1MB
+    }
+});
+/*var server = new Hapi.Server();
+server.connection( { port: 8000 } );*/
 
 
 server.views({
@@ -29,16 +34,6 @@ server.route({
     path: '/',
     handler: function(req, reply) {
         reply.view('view.ejs', {
-            video_id: '',
-            youtube: false
-        });
-    }
-});
-server.route({
-    method: 'GET',
-    path: '/three',
-    handler: function(req, reply) {
-        reply.view('three.ejs', {
             video_id: '',
             youtube: false
         });
