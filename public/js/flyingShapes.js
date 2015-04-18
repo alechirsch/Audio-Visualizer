976f1fb482997bf5e025d9f73c0ff3560f08a154
samdeanefox <samdeanefox@gmail.com>
2015-04-18 17:28:12 -0400

    orbit working

--- public/js/flyingShapes.js
+++ public/js/flyingShapes.js
@@ -3,8 +3,6 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 
 var stats;
 
-var camera, scene;
-
 var parameters;
 
 var SEGMENTS = 512;
@@ -23,10 +21,62 @@ var min = 0;
 var sum = 0;
 var max = 256 * 20;
 
+
+
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+camera.position.z = 15;
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+var geometry = new THREE.BoxGeometry(1,1,1);
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var parent = new THREE.Mesh( geometry, material );
+
+// parent
+//parent = new THREE.Object3D();
+scene.add( parent );
+
+// pivots
+var pivot1 = new THREE.Object3D();
+var pivot2 = new THREE.Object3D();
+var pivot3 = new THREE.Object3D();
+var pivot4 = new THREE.Object3D();
+
+pivot1.rotation.z = 0;
+pivot2.rotation.z = 2 * Math.PI / 4;
+pivot3.rotation.z = 4 * Math.PI / 4;
+pivot4.rotation.z = 6 * Math.PI / 4;
+
+parent.add( pivot1 );
+parent.add( pivot2 );
+parent.add( pivot3 );
+parent.add( pivot4 );
+
+// mesh
+var mesh1 = new THREE.Mesh( geometry, material );
+var mesh2 = new THREE.Mesh( geometry, material );
+var mesh3 = new THREE.Mesh( geometry, material );
+var mesh4 = new THREE.Mesh( geometry, material );
+
+mesh1.position.y = 5;
+mesh2.position.y = 5;
+mesh3.position.y = 5;
+mesh4.position.y = 5;
+
+pivot1.add( mesh1 );
+pivot2.add( mesh2 );
+pivot3.add( mesh3 );
+pivot4.add( mesh4 );
+
+
+
+
+
+
+
 function init() {
-
-
-
 	try {
 		window.AudioContext = window.AudioContext || window.webkitAudioContext;
 		audioContext = new window.AudioContext();
@@ -54,54 +104,8 @@ function init() {
 	analyser.smoothingTimeConstant = 0.000001;
 	analyser.fftSize = 1024;
 
-	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-	camera.position.z = 500;
-	scene = new THREE.Scene();
-	var geometry = new THREE.PlaneBufferGeometry( 2, 2 );
-
-	var innerlayer = new THREE.Group();
-	var outerlayer = new THREE.Group();
-	var allobjects = new THREE.Group();
-
-	//CENTER SPHERE (invisible)
-	var spheregeometry = new THREE.SphereGeometry(20, 16, 16);
-    var spherematerial = new THREE.MeshBasicMaterial( { wireframe: true, color: 0x0000ff } );
-    var sphere = new THREE.Mesh(spheregeometry, spherematerial);
-    sphere.position.set(0, 0, 0);
-    scene.add(sphere);
-    //CUBE
-    //var cubegeometry = new THREE.BoxGeometry(20, 20, 20);
-    //var cubematerial = new THREE.MeshNormalMaterial( { color: 0x00ff00 } );
-    //var cube = new THREE.Mesh(cubegeometry, cubematerial);
-    //cube.position.set(200, 0, 0);
-    //scene.add(cube);
-
-    var lineStart = new THREE.Vector3(0,0,0);
-    var lineEnd = new THREE.Vector3(200,0,0);
-    var linegeometry = new THREE.Geometry();
-    linegeometry.vertices.push(new THREE.Vector3(0,0,0), new THREE.Vector3(200,0,0));
-    var linematerial = new THREE.LineBasicMaterial( { color: 0x000000 } );
-    var line = new THREE.Line(linegeometry, linematerial);
-    scene.add(line);
-    
-
 	parameters = {
 		time: { type: "f", value: 1.0 },
-		//GROUP
-		innergroup: innerlayer,
-		outergroup: outerlayer,
-		entiregroup: allobjects,
-
-		//SPHERE
-		sphereShape: sphere,
-		sphereRotX: 1,
-		sphereRotY: 1,
-		sphereRotZ: 1,
-
-		//LINE
-		lineShape: line,
-		lineGeo: linegeometry,
-		lineRotZ: 1,
 
 		innerRadius: 150,
 		outerRadius: 300,
@@ -109,7 +113,6 @@ function init() {
 	};
 
 	audioInit();
-	sceneInit();
 }
 
 function audioInit(){
@@ -118,44 +121,12 @@ function audioInit(){
 	onParamsChange();
 }
 
-function sceneInit() {
-	var cubegeometry = new THREE.BoxGeometry(20, 20, 20);
-    var cubematerial = new THREE.MeshNormalMaterial( { color: 0x00ff00 } );
-    var cubes = [];
-    for(var i=0; i<8; i++) {
-    	cubes.push(new THREE.Mesh(cubegeometry, cubematerial));
-	}
-	cubes[0].position.set(parameters.innerRadius, 0, 0);
-	cubes[1].position.set(0, parameters.innerRadius, 0);
-	cubes[2].position.set(-1*parameters.innerRadius, 0, 0);
-	cubes[3].position.set(0, -1*parameters.innerRadius, 0);
-	cubes[4].position.set(parameters.outerRadius, 0, 0);
-	cubes[5].position.set(0, parameters.outerRadius, 0);
-	cubes[6].position.set(-1*parameters.outerRadius, 0, 0);
-	cubes[7].position.set(0, -1*parameters.outerRadius, 0);
-	for(var j=0; j<4; j++) {
-		parameters.innergroup.add(cubes[j]);
-		parameters.entiregroup.add(cubes[j]);
-		parameters.sphereShape.add(cubes[j]);
-	}
-	for(var j=0; j<4; j++) {
-		parameters.outergroup.add(cubes[j]);
-		parameters.entiregroup.add(cubes[j]);
-		parameters.sphereShape.add(cubes[j]);
-	}
-	for(var cube in cubes) {
-		scene.add(cubes[cube]);
-	}
+function onParamsChange() {
+
 }
-
-	function onParamsChange() {
-		console.log("I got here");
-		/* when a parameter is changed, change it */
-
-	}
-function normalize(value){
+function normalize(value) {
 		return (value - min) / (max - min);
-	}
+}
 
 function update() {
 
@@ -194,22 +165,8 @@ function animate() {
 var temp = 1;
 function render() {
 	update(source);
-	//SPHERE
-	//parameters.sphereShape.rotation.x += parameters.sphereRotX;
-	//parameters.sphereShape.rotation.y += parameters.sphereRotY;
-	//parameters.sphereShape.rotation.z += parameters.sphereRotZ;
-
-	parameters.sphereShape.traverse(function(obj) {
-		obj.rotation.x += parameters.sphereRotX;
-		obj.rotation.y += parameters.sphereRotY;
-		obj.rotation.z += parameters.sphereRotZ;
-		//console.log(parameters.sphereShape.rotation.x);
-	});
-
 
 	parameters.time.value += 0.05;
 	renderer.render( scene, camera );
-
-	//parameters.lineShape.rotation.z += 0.01;
-
+	parent.rotation.z += 0.01;
 }
